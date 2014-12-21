ec54c3ddeff2672b4307368bd75686b05b2b9f2a
akitsukikiritsugu <240799357@qq.com>
2014-12-21 19:30:47 +0800

    autonightlycommit

--- init.el
+++ init.el
@@ -169,4 +169,34 @@
 ;; Shortcuts
 (global-set-key (kbd "C-x t") 'ansi-term)
 (global-set-key (kbd "C-x j") 'slime)
+(global-set-key (kbd "C-c C-f" 'my-fullscreen))
 (require 'oicq)
+
+;; Full Screen
+;; Full screen
+(defun my-fullscreen ()
+  (interactive)
+  (x-send-client-message
+   nil 0 nil "_NET_WM_STATE" 32
+   '(2 "_NET_WM_STATE_FULLSCREEN" 0)))
+ 
+;; Maximize
+(defun my-maximized-horz ()
+  (interactive)
+  (x-send-client-message
+   nil 0 nil "_NET_WM_STATE" 32
+   '(1 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
+(defun my-maximized-vert ()
+  (interactive)
+  (x-send-client-message
+   nil 0 nil "_NET_WM_STATE" 32
+   '(1 "_NET_WM_STATE_MAXIMIZED_VERT" 0)))
+(defun my-maximized ()
+  (interactive)
+  (x-send-client-message
+   nil 0 nil "_NET_WM_STATE" 32
+   '(1 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+  (interactive)
+  (x-send-client-message
+   nil 0 nil "_NET_WM_STATE" 32
+   '(1 "_NET_WM_STATE_MAXIMIZED_VERT" 0)))
