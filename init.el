de6a6c5b8485c5bb49cda26de2235d39850c11d4
Kirito Akitsuki <240799357@qq.com>
2017-03-27 23:31:43 +1100

    Fix some minor mitakes on Mon Mar 27 23:31:43 AEDT 2017

--- init.el
+++ init.el
@@ -86,7 +86,7 @@
 ;(require 'init-rails)
 (require 'init-sql)
 
-(require 'init-paredit)
+;(require 'init-paredit)
 (require 'init-lisp)
 (require 'init-slime)
 ;(require 'init-clojure)
@@ -201,6 +201,64 @@
    nil 0 nil "_NET_WM_STATE" 32
    '(1 "_NET_WM_STATE_MAXIMIZED_VERT" 0)))
 
+
 ;; Convert from fullscreen and muiltple types of windows is defualt bind to C-M-123456789 in keypad.
 
 
+;; Configure scheme
+
+(require 'cmuscheme)
+(setq scheme-program-name "racket")
+;; bypass the interactive question and start the default interpreter
+(defun scheme-proc ()
+  "Return the current Scheme process, starting one if necessary."
+  (unless (and scheme-buffer
+               (get-buffer scheme-buffer)
+               (comint-check-proc scheme-buffer))
+    (save-window-excursion
+      (run-scheme scheme-program-name)))
+  (or (scheme-get-process)
+      (error "No current process. See variable `scheme-buffer'")))
+(defun scheme-split-window ()
+  (cond
+   ((= 1 (count-windows))
+    (delete-other-windows)
+    (split-window-vertically (floor (* 0.68 (window-height))))
+    (other-window 1)
+    (switch-to-buffer "*scheme*")
+    (other-window 1))
+   ((not (find "*scheme*"
+               (mapcar (lambda (w) (buffer-name (window-buffer w)))
+                       (window-list))
+               :test 'equal))
+    (other-window 1)
+    (switch-to-buffer "*scheme*")
+    (other-window -1))))
+(defun scheme-send-last-sexp-split-window ()
+  (interactive)
+  (scheme-split-window)
+  (scheme-send-last-sexp))
+(defun scheme-send-definition-split-window ()
+  (interactive)
+  (scheme-split-window)
+  (scheme-send-definition))
+
+(add-hook 'scheme-mode-hook
+          (lambda ()
+            (paredit-mode 1)
+            (define-key scheme-mode-map (kbd "<f5>") 'scheme-send-last-sexp-split-window)
+            (define-key scheme-mode-map (kbd "<f6>") 'scheme-send-definition-split-window)))
+
+;;new paredit
+(autoload 'paredit-mode "paredit"
+  "Minor mode for pseudo-structurally editing Lisp code."
+  t)
+
+;; lpr driver are not suitable for Brother HL1210W. Change the lpr-command to xpp and explicit the printer name.
+(setq lpr-command "lp")
+;; (setq printer-name "HL1210W")
+(setq lpr-add-switches '())
+;; (setq lpr-switches "-d\ HL1210W")
+
+(provide 'init)
+;;; init.el ends here
