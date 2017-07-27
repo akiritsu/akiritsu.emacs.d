72370da107769ae6e1c003ef213db95ee1f86e9e
Kirito Akitsuki <240799357@qq.com>
2017-07-27 21:09:44 +1000

    Minor changes on Thu Jul 27 21:09:44 AEST 2017

--- init.el
+++ init.el
@@ -172,7 +172,7 @@
 (global-set-key (kbd "C-x t") 'shell)
 (global-set-key (kbd "C-x j") 'slime)
 ;; hs-minor-mode
-(global-set-key (kbd "C-,") 'hs-troggle-hiding)
+(global-set-key (kbd "C-,") 'hs-toggle-hiding)
 
 ;; Configure scheme
 
