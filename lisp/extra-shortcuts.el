a2d4270c088e7cc5bc883dd7c816f5a824907d35
Kirito Akitsuki <240799357@qq.com>
2017-07-31 09:46:40 +1000

    Minor changes on Mon Jul 31 09:46:40 AEST 2017

--- lisp/extra-shortcuts.el
+++ lisp/extra-shortcuts.el
@@ -1,7 +1,8 @@
 (global-set-key (kbd "C-x t") 'shell)
 (global-set-key (kbd "C-x j") 'slime)
-;; hs-minor-mode
+
 (global-set-key (kbd "C-,") 'origami-recursively-toggle-node)
 
+(global-set-key (kbd "C-M-;") 'comment-box)
 
 (provide 'extra-shortcuts)
