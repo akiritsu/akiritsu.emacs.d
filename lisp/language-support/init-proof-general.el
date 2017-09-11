9b37625a9b6707b9e9a9d49daa6b0bdb657442a1
akiritsu <240799357@qq.com>
2017-09-12 01:36:58 +1000

    support oz

---
+++ lisp/language-support/init-proof-general.el
@@ -0,0 +1,23 @@
+;;; Use Proof General (https://github.com/ProofGeneral/PG) to support Coq.
+
+;;; Online Documentation: https://proofgeneral.github.io/doc/
+
+;;; Prover Extensions Mode
+;;; LEGO .l lego-mode
+;;; Coq .v coq-mode
+;;; Isabelle .thy isar-mode
+;;; Phox .phx phox-mode
+;;; HOL98 .sml hol98-mode
+;;; HOL Light .ml hol-light-mode
+;;; ACL2 .acl2 acl2-mode
+;;; Twelf .elf twelf-mode
+;;; Plastic .lf plastic-mode
+;;; Lambda-CLAM .lcm lclam-mode
+;;; CCC .ccc ccc-mode
+;;; PG-Shell .pgsh pgshell-mode
+
+
+(load "~/.emacs.d/site-lisp/proof-general/generic/proof-site")
+
+
+(provide 'init-proof-general)
